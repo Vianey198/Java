91c77fe72a6ebf63101b471ba959c3eae8b5b9ff
Vianey cante <viane.cante@hotmail.com>
2024-02-27 10:50:09 -0600

    Versión original

---
+++ Nombre.java
@@ -0,0 +1,15 @@
+package EjNombre;
+
+import java.util.Scanner;
+
+public class Nombre {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Hola Vianey Guadalupe");
+	 	
+	}
+
+}
